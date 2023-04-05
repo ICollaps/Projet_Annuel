bf4c1baedcd101d9d6b3978b4c6307e8c62967d6
ICollaps <collaspierre51@gmail.com>
2023-04-05 20:31:16 +0200

    First working API

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template , request
+import joblib
 
 app = Flask(__name__)
 
-@app.route('/health')
-def health():
-    return 'Hello, bro!'
+model = joblib.load("model/trained_models/best_model.pkl")
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/health')
+def health():
+    return render_template('form.html')
+
+@app.route('/health/predict', methods=['POST'])
+def predict():
+    input_data = request.form['input_data']
+    input_values = list(map(float, input_data.split(',')))
+
+    prediction = model.predict([input_values])[0]
+    return f'La prédiction du modèle est : {prediction}'
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
