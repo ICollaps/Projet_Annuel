d5e4ac84faf6c3928846eb186f4a2aa863ae4262
ICollaps <collaspierre51@gmail.com>
2023-04-06 23:32:09 +0200

    Adding Welcome page and authentification system

--- app.py
+++ app.py
@@ -1,28 +1,103 @@
-from flask import Flask, redirect, url_for, render_template , request
+from flask import Flask, redirect, url_for, render_template , request , flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 import joblib
 
 app = Flask(__name__)
+
+app.config['LOGIN_VIEW'] = 'login'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = 'some_secret_key'
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            flash('Vous êtes connecté avec succès !', 'success')
+            return redirect(url_for('health'))
+        else:
+            flash("Nom d'utilisateur ou mot de passe incorrect", 'danger')
+            
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Vous êtes déconnecté avec succès.', 'success')
+    return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='sha256')
+        
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash("Nom d'utilisateur déjà pris, veuillez en choisir un autre.", 'danger')
+            return redirect(url_for('register'))
+
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Inscription réussie ! Veuillez vous connecter.", 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+
+
+
+
+
+
+
+
 
 model = joblib.load("model/trained_models/best_model.pkl")
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('welcome.html')
 
 
-@app.route('/submit_form', methods=['POST'])
-def submit_form():
-    # Vous pouvez ajouter du code ici pour traiter les données du formulaire si nécessaire
-    return redirect(url_for('health'))
 
 
 
 @app.route('/health')
+@login_required
 def health():
     return render_template('user_input.html')
 
 @app.route('/health/predict', methods=['POST'])
+@login_required
 def predict():
     PRG = float(request.form['PRG'])
     PL = float(request.form['PL'])
@@ -45,4 +120,6 @@ def predict():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
